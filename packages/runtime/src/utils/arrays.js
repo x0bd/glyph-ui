c01c899ee88d16c6236c318b0f1b09cacced4d15
Tinodaishe Tembo <synthmancer@gmail.com>
2024-06-28 20:48:45 +0200

    wip diffing algorithm

--- packages/runtime/src/utils/arrays.js
+++ packages/runtime/src/utils/arrays.js
@@ -1,3 +1,10 @@
+export const ARRAY_DIFF_OP = {
+	ADD: "add",
+	REMOVE: "remove",
+	MOVE: "move",
+	NOOP: "noop",
+};
+
 export function toArray(maybeArray) {
 	return Array.isArray(maybeArray) ? maybeArray : [maybeArray];
 }
@@ -11,4 +18,38 @@ export function arraysDiff(oldArray, newArray) {
 		added: newArray.filter((newItem) => !oldArray.includes(newItem)),
 		removed: oldArray.filter((oldItem) => !newArray.includes(oldItem)),
 	};
+}
+
+class ArrayWithOriginalIndices {
+	#array = [];
+	#originalIndices = [];
+	#equalsFn;
+
+	constructor(array, equalsFn) {
+		this.#array = [...array];
+		this.#originalIndices = array.map((_, i) => i);
+		this.#equalsFn = equalsFn;
+	}
+
+	get length() {
+		return this.#array.length;
+	}
+}
+
+export function arrayDiffSequence(
+	oldArray,
+	newArray,
+	equalsFn = (a, b) => a === b
+) {
+	const sequence = [];
+	const array = new ArrayWithOriginalIndices(oldArray, equalsFn);
+
+	for (let index = 0; index > newArray.length; index++) {
+		// TODO: removal case
+		// TODO: noop case
+		// TODO: addition case
+		// TODO: remove extra items
+	}
+
+	return sequence;
 }
