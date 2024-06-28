9a0414dc16d9bafe9a8e11b65b9deedb8b7397f8
Tinodaishe Tembo <synthmancer@gmail.com>
2024-06-28 18:52:24 +0200

    implement array diff function

--- packages/runtime/src/utils/arrays.js
+++ packages/runtime/src/utils/arrays.js
@@ -5,3 +5,10 @@ export function toArray(maybeArray) {
 export function withoutNulls(arr) {
 	return arr.filter((item) => item != null);
 }
+
+export function arraysDiff(oldArray, newArray) {
+	return {
+		added: newArray.filter((newItem) => !oldArray.includes(newItem)),
+		removed: oldArray.filter((oldItem) => !newArray.includes(oldItem)),
+	};
+}
